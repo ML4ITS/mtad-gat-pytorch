054a1dcce5500c3e3e723a159d79e57f8c44b566
axeloh <axel.harstad@gmail.com>
2021-03-10 17:50:30 +0100

    model w/o temp attn

--- mtad_gat.py
+++ mtad_gat.py
@@ -38,12 +38,12 @@ class MTAD_GAT(nn.Module):
 		# x shape (b, n, k): b - batch size, n - window size, k - number of nodes/features
 		x = self.conv(x)
 
-		#h_feat = self.feature_gat(x)
-		h_temp = self.temporal_gat(x)
+		h_feat = self.feature_gat(x)
+		#h_temp = self.temporal_gat(x)
 
 		#h_cat = torch.cat([x, h_feat.permute(0, 2, 1), h_temp], dim=2) # (b, n, 3k)
 
-		h_cat = torch.cat([x, h_temp], dim=2)
+		h_cat = torch.cat([x, h_feat.permute(0, 2, 1)], dim=2)
 
 		gru_out, h_end = self.gru(h_cat)  # (
 
