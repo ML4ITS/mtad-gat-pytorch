61088f853d3f273b8c4bb51eec254f0f286df841
axeloh <axel.harstad@gmail.com>
2021-03-10 17:40:50 +0100

    model w/o feat attn

--- mtad_gat.py
+++ mtad_gat.py
@@ -23,7 +23,7 @@ class MTAD_GAT(nn.Module):
 		self.conv = ConvLayer(n_features, window_size, kernel_size, device)
 		self.feature_gat = FeatureAttentionLayer(n_features, window_size, dropout, alpha, device)
 		self.temporal_gat = TemporalAttentionLayer(n_features, window_size, dropout, alpha, device)
-		self.gru = GRU(3*n_features, gru_hid_dim, gru_n_layers, dropout, device)
+		self.gru = GRU(2*n_features, gru_hid_dim, gru_n_layers, dropout, device)
 		self.forecasting_model = Forecasting_Model(gru_hid_dim, forecast_hid_dim, out_dim*horizon, forecast_n_layers, dropout, device)
 		self.recon_model = RNNAutoencoder(window_size, gru_hid_dim, autoenc_n_layers, autoenc_hid_dim, n_features, dropout, device)
 
@@ -38,10 +38,12 @@ class MTAD_GAT(nn.Module):
 		# x shape (b, n, k): b - batch size, n - window size, k - number of nodes/features
 		x = self.conv(x)
 
-		h_feat = self.feature_gat(x)
+		#h_feat = self.feature_gat(x)
 		h_temp = self.temporal_gat(x)
 
-		h_cat = torch.cat([x, h_feat.permute(0, 2, 1), h_temp], dim=2) # (b, n, 3k)
+		#h_cat = torch.cat([x, h_feat.permute(0, 2, 1), h_temp], dim=2) # (b, n, 3k)
+
+		h_cat = torch.cat([x, h_temp], dim=2)
 
 		gru_out, h_end = self.gru(h_cat)  # (
 
