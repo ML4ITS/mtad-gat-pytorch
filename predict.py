b44ec6e77862ec8e006c8a51a98f49bf50f2ae11
axeloh <axel.harstad@gmail.com>
2021-03-15 19:02:13 +0100

    fix

--- predict.py
+++ predict.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
 					 use_cuda=model_args.use_cuda)
 
 	device = 'cuda' if args.use_cuda and torch.cuda.is_available() else 'cpu'
-	load(model, f'{pre_trained_model_path}_model.pt', device=device)
+	model = load(model, f'{pre_trained_model_path}_model.pt', device=device)
 
 	predictor = Predictor(model, window_size, n_features, level=level, save_path=output_path)
 	predictor.predict_anomalies(x_train, x_test, label, save_scores=save_scores, load_scores=load_scores)
