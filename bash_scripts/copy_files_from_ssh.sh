6f2b662112345578d4fa97cac2ed3efb7258823c
axeloh <axel.harstad@gmail.com>
2021-03-26 21:00:03 +0100

    results from all experiments, plotting, ++

--- bash_scripts/copy_files_from_ssh.sh
+++ bash_scripts/copy_files_from_ssh.sh
@@ -7,15 +7,15 @@ scp -P $port -r $ssh:~/MTAD-GAT/models ./
 
 for i in {1..8}
 do
-    scp -P $port -r $ssh:~/MTAD-GAT/output/smd/1-$i ./output/smd/
+    scp -P $port -r $ssh:~/MTAD-GAT/output/SMD/1-$i ./output/SMD/
 done
 
 for i in {1..9}
 do
-    scp -P $port -r $ssh:~/MTAD-GAT/output/smd/2-$i ./output/smd/
+    scp -P $port -r $ssh:~/MTAD-GAT/output/SMD/2-$i ./output/SMD/
 done
 
 for i in {1..11}
 do
-    scp -P $port -r $ssh:~/MTAD-GAT/output/smd/3-$i ./output/smd/
+    scp -P $port -r $ssh:~/MTAD-GAT/output/SMD/3-$i ./output/SMD/
 done
