67bee4d58693ed8c1a723ef8a599e224a2735f47
wQuole <wgkvaale@gmail.com>
2021-05-05 17:03:06 +0200

    fix target dims

--- utils.py
+++ utils.py
@@ -59,7 +59,7 @@ def get_target_dims(dataset):
 	elif dataset == "SMD":
 		return None
 	elif dataset == "TELENOR":
-		return [4, 15, 26]
+		return None
 	else:
 		raise ValueError("unknown dataset " + str(dataset))
 
