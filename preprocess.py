5d423c8075256a3f891861330f4914d4d2609e3e
srigas <rigassp@gmail.com>
2023-05-09 13:39:39 +0300

    removing old datasets (preprocess, args)

--- preprocess.py
+++ preprocess.py
@@ -1,93 +1,33 @@
-from ast import literal_eval
-from csv import reader
-from os import listdir, makedirs, path
+from os import makedirs, path
 from pickle import dump
 import numpy as np
 
 from args import get_parser
 
-
-def load_and_save(category, filename, dataset, dataset_folder, output_folder):
+# Loads the data from the .txt files and saves them into pickled objects
+def load_and_save(filename, dataset, output_folder):
     temp = np.genfromtxt(
-        path.join(dataset_folder, category, filename),
+        path.join(dataset, filename),
         dtype=np.float32,
         delimiter=",",
     )
-    print(dataset, category, filename, temp.shape)
-    with open(path.join(output_folder, dataset + "_" + category + ".pkl"), "wb") as file:
+    print(f"{filename}: {temp.shape}")
+    with open(path.join(output_folder, filename+".pkl"), "wb") as file:
         dump(temp, file)
 
-
+# Each dataset should in general have three files:
+# 1. train.txt -> contains csv of features for training
+# 2. test.txt -> contains csv of features for evaluation
+# 3. labels.txt -> contains the labels of the test data
 def load_data(dataset):
-    """ Method from OmniAnomaly (https://github.com/NetManAIOps/OmniAnomaly) """
-
-    if dataset == "SMD":
-        dataset_folder = "datasets/ServerMachineDataset"
-        output_folder = "datasets/ServerMachineDataset/processed"
-        makedirs(output_folder, exist_ok=True)
-        file_list = listdir(path.join(dataset_folder, "train"))
-        for filename in file_list:
-            if filename.endswith(".txt"):
-                load_and_save(
-                    "train",
-                    filename,
-                    filename.strip(".txt"),
-                    dataset_folder,
-                    output_folder,
-                )
-                load_and_save(
-                    "test_label",
-                    filename,
-                    filename.strip(".txt"),
-                    dataset_folder,
-                    output_folder,
-                )
-                load_and_save(
-                    "test",
-                    filename,
-                    filename.strip(".txt"),
-                    dataset_folder,
-                    output_folder,
-                )
-
-    elif dataset == "SMAP" or dataset == "MSL":
-        dataset_folder = "datasets/data"
-        output_folder = "datasets/data/processed"
-        makedirs(output_folder, exist_ok=True)
-        with open(path.join(dataset_folder, "labeled_anomalies.csv"), "r") as file:
-            csv_reader = reader(file, delimiter=",")
-            res = [row for row in csv_reader][1:]
-        res = sorted(res, key=lambda k: k[0])
-        data_info = [row for row in res if row[1] == dataset and row[0] != "P-2"]
-        labels = []
-        for row in data_info:
-            anomalies = literal_eval(row[2])
-            length = int(row[-1])
-            label = np.zeros([length], dtype=np.bool_)
-            for anomaly in anomalies:
-                label[anomaly[0] : anomaly[1] + 1] = True
-            labels.extend(label)
-
-        labels = np.asarray(labels)
-        print(dataset, "test_label", labels.shape)
-
-        with open(path.join(output_folder, dataset + "_" + "test_label" + ".pkl"), "wb") as file:
-            dump(labels, file)
-
-        def concatenate_and_save(category):
-            data = []
-            for row in data_info:
-                filename = row[0]
-                temp = np.load(path.join(dataset_folder, category, filename + ".npy"))
-                data.extend(temp)
-            data = np.asarray(data)
-            print(dataset, category, data.shape)
-            with open(path.join(output_folder, dataset + "_" + category + ".pkl"), "wb") as file:
-                dump(data, file)
-
-        for c in ["train", "test"]:
-            concatenate_and_save(c)
-
+    dataset_folder = path.join("datasets", dataset)
+    output_folder = path.join(dataset_folder,"processsed")
+    # Make the directory if it does not exist
+    makedirs(output_folder, exist_ok=True)
+    # Load the 3 files and save them
+    load_and_save("train.txt", dataset_folder, output_folder)
+    load_and_save("test.txt", dataset_folder, output_folder)
+    load_and_save("labels.txt", dataset_folder, output_folder)
 
 if __name__ == "__main__":
     parser = get_parser()
