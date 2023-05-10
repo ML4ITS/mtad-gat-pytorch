c5f345e3c27fb915349eb66f2d53ac8cbcf73b46
srigas <rigassp@gmail.com>
2023-05-10 13:55:09 +0300

    insignificant changes

--- training.py
+++ training.py
@@ -1,242 +1,201 @@
-import os
-import time
-import numpy as np
-import torch
-import torch.nn as nn
-from torch.utils.tensorboard import SummaryWriter
+import json
+from tqdm import tqdm
+from eval_methods import *
+from utils import *
 
 
-class Trainer:
-    """Trainer class for MTAD-GAT model.
+class Predictor:
+    """MTAD-GAT predictor class.
 
-    :param model: MTAD-GAT model
-    :param optimizer: Optimizer used to minimize the loss function
+    :param model: MTAD-GAT model (pre-trained) used to forecast and reconstruct
     :param window_size: Length of the input sequence
     :param n_features: Number of input features
-    :param n_epochs: Number of iterations/epochs
-    :param batch_size: Number of windows in a single batch
-    :param init_lr: Initial learning rate of the module
-    :param forecast_criterion: Loss to be used for forecasting.
-    :param recon_criterion: Loss to be used for reconstruction.
-    :param boolean use_cuda: To be run on GPU or not
-    :param dload: Download directory where models are to be dumped
-    :param log_dir: Directory where SummaryWriter logs are written to
-    :param print_every: At what epoch interval to print losses
-    :param log_tensorboard: Whether to log loss++ to tensorboard
-    :param args_summary: Summary of args that will also be written to tensorboard if log_tensorboard
+    :param pred_args: params for thresholding and predicting anomalies
+
     """
 
-    def __init__(
-        self,
-        model,
-        optimizer,
-        window_size,
-        n_features,
-        n_epochs=200,
-        batch_size=256,
-        init_lr=0.001,
-        forecast_criterion=nn.MSELoss(),
-        recon_criterion=nn.MSELoss(),
-        use_cuda=True,
-        dload="",
-        log_dir="output/",
-        print_every=1,
-        log_tensorboard=True,
-        args_summary="",
-    ):
-
+    def __init__(self, model, window_size, n_features, pred_args, summary_file_name="summary.txt"):
         self.model = model
-        self.optimizer = optimizer
         self.window_size = window_size
         self.n_features = n_features
-        self.n_epochs = n_epochs
-        self.batch_size = batch_size
-        self.init_lr = init_lr
-        self.forecast_criterion = forecast_criterion
-        self.recon_criterion = recon_criterion
-        self.device = "cuda" if use_cuda and torch.cuda.is_available() else "cpu"
-        self.dload = dload
-        self.log_dir = log_dir
-        self.print_every = print_every
-        self.log_tensorboard = log_tensorboard
+        self.dataset = pred_args["dataset"]
+        self.target_dims = pred_args["target_dims"]
+        self.scale_scores = pred_args["scale_scores"]
+        self.q = pred_args["q"]
+        self.level = pred_args["level"]
+        self.dynamic_pot = pred_args["dynamic_pot"]
+        self.use_mov_av = pred_args["use_mov_av"]
+        self.gamma = pred_args["gamma"]
+        self.reg_level = pred_args["reg_level"]
+        self.save_path = pred_args["save_path"]
+        self.batch_size = 256
+        self.use_cuda = True
+        self.pred_args = pred_args
+        self.summary_file_name = summary_file_name
 
-        self.losses = {
-            "train_total": [],
-            "train_forecast": [],
-            "train_recon": [],
-            "val_total": [],
-            "val_forecast": [],
-            "val_recon": [],
-        }
-        self.epoch_times = []
-
-        if self.device == "cuda":
-            self.model.cuda()
-
-        if self.log_tensorboard:
-            self.writer = SummaryWriter(f"{log_dir}")
-            self.writer.add_text("args_summary", args_summary)
-
-    def fit(self, train_loader, val_loader=None):
-        """Train model for self.n_epochs.
-        Train and validation (if validation loader given) losses stored in self.losses
-
-        :param train_loader: train loader of input data
-        :param val_loader: validation loader of input data
+    def get_score(self, values):
+        """Method that calculates anomaly score using given model and data
+        :param values: 2D array of multivariate time series data, shape (N, k)
+        :return np array of anomaly scores + dataframe with prediction for each channel and global anomalies
         """
 
-        init_train_loss = self.evaluate(train_loader)
-        print(f"Init total train loss: {init_train_loss[2]:5f}")
-
-        if val_loader is not None:
-            init_val_loss = self.evaluate(val_loader)
-            print(f"Init total val loss: {init_val_loss[2]:.5f}")
-
-        print(f"Training model for {self.n_epochs} epochs..")
-        train_start = time.time()
-        for epoch in range(self.n_epochs):
-            epoch_start = time.time()
-            self.model.train()
-            forecast_b_losses = []
-            recon_b_losses = []
-
-            for x, y in train_loader:
-                x = x.to(self.device)
-                y = y.to(self.device)
-                self.optimizer.zero_grad()
-
-                preds, recons = self.model(x)
-
-                if preds.ndim == 3:
-                    preds = preds.squeeze(1)
-                if y.ndim == 3:
-                    y = y.squeeze(1)
-
-                forecast_loss = torch.sqrt(self.forecast_criterion(y, preds))
-                recon_loss = torch.sqrt(self.recon_criterion(x, recons))
-                loss = forecast_loss + recon_loss
-
-                loss.backward()
-                self.optimizer.step()
-
-                forecast_b_losses.append(forecast_loss.item())
-                recon_b_losses.append(recon_loss.item())
-
-            forecast_b_losses = np.array(forecast_b_losses)
-            recon_b_losses = np.array(recon_b_losses)
-
-            forecast_epoch_loss = np.sqrt((forecast_b_losses ** 2).mean())
-            recon_epoch_loss = np.sqrt((recon_b_losses ** 2).mean())
-
-            total_epoch_loss = forecast_epoch_loss + recon_epoch_loss
-
-            self.losses["train_forecast"].append(forecast_epoch_loss)
-            self.losses["train_recon"].append(recon_epoch_loss)
-            self.losses["train_total"].append(total_epoch_loss)
-
-            # Evaluate on validation set
-            forecast_val_loss, recon_val_loss, total_val_loss = "NA", "NA", "NA"
-            if val_loader is not None:
-                forecast_val_loss, recon_val_loss, total_val_loss = self.evaluate(val_loader)
-                self.losses["val_forecast"].append(forecast_val_loss)
-                self.losses["val_recon"].append(recon_val_loss)
-                self.losses["val_total"].append(total_val_loss)
-
-                if total_val_loss <= self.losses["val_total"][-1]:
-                    self.save(f"model.pt")
-
-            if self.log_tensorboard:
-                self.write_loss(epoch)
-
-            epoch_time = time.time() - epoch_start
-            self.epoch_times.append(epoch_time)
-
-            if epoch % self.print_every == 0:
-                s = (
-                    f"[Epoch {epoch + 1}] "
-                    f"forecast_loss = {forecast_epoch_loss:.5f}, "
-                    f"recon_loss = {recon_epoch_loss:.5f}, "
-                    f"total_loss = {total_epoch_loss:.5f}"
-                )
-
-                if val_loader is not None:
-                    s += (
-                        f" ---- val_forecast_loss = {forecast_val_loss:.5f}, "
-                        f"val_recon_loss = {recon_val_loss:.5f}, "
-                        f"val_total_loss = {total_val_loss:.5f}"
-                    )
-
-                s += f" [{epoch_time:.1f}s]"
-                print(s)
-
-        if val_loader is None:
-            self.save(f"model.pt")
-
-        train_time = int(time.time() - train_start)
-        if self.log_tensorboard:
-            self.writer.add_text("total_train_time", str(train_time))
-        print(f"-- Training done in {train_time}s.")
-
-    def evaluate(self, data_loader):
-        """Evaluate model
-
-        :param data_loader: data loader of input data
-        :return forecasting loss, reconstruction loss, total loss
-        """
+        print("Predicting and calculating anomaly scores..")
+        data = SlidingWindowDataset(values, self.window_size, self.target_dims)
+        loader = torch.utils.data.DataLoader(data, batch_size=self.batch_size, shuffle=False)
+        device = "cuda" if self.use_cuda and torch.cuda.is_available() else "cpu"
 
         self.model.eval()
-
-        forecast_losses = []
-        recon_losses = []
-
+        preds = []
+        recons = []
         with torch.no_grad():
-            for x, y in data_loader:
-                x = x.to(self.device)
-                y = y.to(self.device)
+            for x, y in tqdm(loader):
+                x = x.to(device)
+                y = y.to(device)
 
-                preds, recons = self.model(x)
+                y_hat, _ = self.model(x)
 
-                if preds.ndim == 3:
-                    preds = preds.squeeze(1)
-                if y.ndim == 3:
-                    y = y.squeeze(1)
+                # Shifting input to include the observed value (y) when doing the reconstruction
+                recon_x = torch.cat((x[:, 1:, :], y), dim=1)
+                _, window_recon = self.model(recon_x)
 
-                forecast_loss = torch.sqrt(self.forecast_criterion(y, preds))
-                recon_loss = torch.sqrt(self.recon_criterion(x, recons))
+                preds.append(y_hat.detach().cpu().numpy())
+                # Extract last reconstruction only
+                recons.append(window_recon[:, -1, :].detach().cpu().numpy())
 
-                forecast_losses.append(forecast_loss.item())
-                recon_losses.append(recon_loss.item())
+        preds = np.concatenate(preds, axis=0)
+        recons = np.concatenate(recons, axis=0)
+        actual = values.detach().cpu().numpy()[self.window_size:]
 
-        forecast_losses = np.array(forecast_losses)
-        recon_losses = np.array(recon_losses)
+        if self.target_dims is not None:
+            actual = actual[:, self.target_dims]
 
-        forecast_loss = np.sqrt((forecast_losses ** 2).mean())
-        recon_loss = np.sqrt((recon_losses ** 2).mean())
+        anomaly_scores = np.zeros_like(actual)
+        df = pd.DataFrame()
+        for i in range(preds.shape[1]):
+            df[f"Forecast_{i}"] = preds[:, i]
+            df[f"Recon_{i}"] = recons[:, i]
+            df[f"True_{i}"] = actual[:, i]
+            a_score = np.sqrt((preds[:, i] - actual[:, i]) ** 2) + self.gamma * np.sqrt(
+                (recons[:, i] - actual[:, i]) ** 2)
 
-        total_loss = forecast_loss + recon_loss
+            if self.scale_scores:
+                q75, q25 = np.percentile(a_score, [75, 25])
+                iqr = q75 - q25
+                median = np.median(a_score)
+                a_score = (a_score - median) / (1+iqr)
 
-        return forecast_loss, recon_loss, total_loss
+            anomaly_scores[:, i] = a_score
+            df[f"A_Score_{i}"] = a_score
 
-    def save(self, file_name):
+        anomaly_scores = np.mean(anomaly_scores, 1)
+        df['A_Score_Global'] = anomaly_scores
+
+        return df
+
+    def predict_anomalies(self, train, test, true_anomalies, load_scores=False, save_output=True,
+                          scale_scores=False):
+        """ Predicts anomalies
+
+        :param train: 2D array of train multivariate time series data
+        :param test: 2D array of test multivariate time series data
+        :param true_anomalies: true anomalies of test set, None if not available
+        :param save_scores: Whether to save anomaly scores of train and test
+        :param load_scores: Whether to load anomaly scores instead of calculating them
+        :param save_output: Whether to save output dataframe
+        :param scale_scores: Whether to feature-wise scale anomaly scores
         """
-        Pickles the model parameters to be retrieved later
-        :param file_name: the filename to be saved as,`dload` serves as the download directory
-        """
-        PATH = self.dload + "/" + file_name
-        if os.path.exists(self.dload):
-            pass
+
+        if load_scores:
+            print("Loading anomaly scores")
+
+            train_pred_df = pd.read_pickle(f"{self.save_path}/train_output.pkl")
+            test_pred_df = pd.read_pickle(f"{self.save_path}/test_output.pkl")
+
+            train_anomaly_scores = train_pred_df['A_Score_Global'].values
+            test_anomaly_scores = test_pred_df['A_Score_Global'].values
+
         else:
-            os.mkdir(self.dload)
-        torch.save(self.model.state_dict(), PATH)
+            train_pred_df = self.get_score(train)
+            test_pred_df = self.get_score(test)
 
-    def load(self, PATH):
-        """
-        Loads the model's parameters from the path mentioned
-        :param PATH: Should contain pickle file
-        """
-        self.model.load_state_dict(torch.load(PATH, map_location=self.device))
+            train_anomaly_scores = train_pred_df['A_Score_Global'].values
+            test_anomaly_scores = test_pred_df['A_Score_Global'].values
 
-    def write_loss(self, epoch):
-        for key, value in self.losses.items():
-            if len(value) != 0:
-                self.writer.add_scalar(key, value[-1], epoch)
+            train_anomaly_scores = adjust_anomaly_scores(train_anomaly_scores, self.dataset, True, self.window_size)
+            test_anomaly_scores = adjust_anomaly_scores(test_anomaly_scores, self.dataset, False, self.window_size)
+
+            # Update df
+            train_pred_df['A_Score_Global'] = train_anomaly_scores
+            test_pred_df['A_Score_Global'] = test_anomaly_scores
+
+        if self.use_mov_av:
+            smoothing_window = int(self.batch_size * self.window_size * 0.05)
+            train_anomaly_scores = pd.DataFrame(train_anomaly_scores).ewm(span=smoothing_window).mean().values.flatten()
+            test_anomaly_scores = pd.DataFrame(test_anomaly_scores).ewm(span=smoothing_window).mean().values.flatten()
+
+        # Find threshold and predict anomalies at feature-level (for plotting and diagnosis purposes)
+        out_dim = self.n_features if self.target_dims is None else len(self.target_dims)
+        all_preds = np.zeros((len(test_pred_df), out_dim))
+        for i in range(out_dim):
+            train_feature_anom_scores = train_pred_df[f"A_Score_{i}"].values
+            test_feature_anom_scores = test_pred_df[f"A_Score_{i}"].values
+            epsilon = find_epsilon(train_feature_anom_scores, reg_level=2)
+
+            train_feature_anom_preds = (train_feature_anom_scores >= epsilon).astype(int)
+            test_feature_anom_preds = (test_feature_anom_scores >= epsilon).astype(int)
+
+            train_pred_df[f"A_Pred_{i}"] = train_feature_anom_preds
+            test_pred_df[f"A_Pred_{i}"] = test_feature_anom_preds
+
+            train_pred_df[f"Thresh_{i}"] = epsilon
+            test_pred_df[f"Thresh_{i}"] = epsilon
+
+            all_preds[:, i] = test_feature_anom_preds
+
+        # Global anomalies (entity-level) are predicted using aggregation of anomaly scores across all features
+        # These predictions are used to evaluate performance, as true anomalies are labeled at entity-level
+        # Evaluate using different threshold methods: brute-force, epsilon and peaks-over-treshold
+        e_eval = epsilon_eval(train_anomaly_scores, test_anomaly_scores, true_anomalies, reg_level=self.reg_level)
+        p_eval = pot_eval(train_anomaly_scores, test_anomaly_scores, true_anomalies,
+                          q=self.q, level=self.level, dynamic=self.dynamic_pot)
+        if true_anomalies is not None:
+            bf_eval = bf_search(test_anomaly_scores, true_anomalies, start=0.01, end=2, step_num=100, verbose=False)
+        else:
+            bf_eval = {}
+
+        print(f"Results using epsilon method:\n {e_eval}")
+        print(f"Results using peak-over-threshold method:\n {p_eval}")
+        print(f"Results using best f1 score search:\n {bf_eval}")
+
+        for k, v in e_eval.items():
+            if not type(e_eval[k]) == list:
+                e_eval[k] = float(v)
+        for k, v in p_eval.items():
+            if not type(p_eval[k]) == list:
+                p_eval[k] = float(v)
+        for k, v in bf_eval.items():
+            bf_eval[k] = float(v)
+
+        # Save
+        summary = {"epsilon_result": e_eval, "pot_result": p_eval, "bf_result": bf_eval}
+        with open(f"{self.save_path}/{self.summary_file_name}", "w") as f:
+            json.dump(summary, f, indent=2)
+
+        # Save anomaly predictions made using epsilon method (could be changed to pot or bf-method)
+        if save_output:
+            global_epsilon = e_eval["threshold"]
+            test_pred_df["A_True_Global"] = true_anomalies
+            train_pred_df["Thresh_Global"] = global_epsilon
+            test_pred_df["Thresh_Global"] = global_epsilon
+            train_pred_df[f"A_Pred_Global"] = (train_anomaly_scores >= global_epsilon).astype(int)
+            test_preds_global = (test_anomaly_scores >= global_epsilon).astype(int)
+            # Adjust predictions according to evaluation strategy
+            if true_anomalies is not None:
+                test_preds_global = adjust_predicts(None, true_anomalies, global_epsilon, pred=test_preds_global)
+            test_pred_df[f"A_Pred_Global"] = test_preds_global
+
+            print(f"Saving output to {self.save_path}/<train/test>_output.pkl")
+            train_pred_df.to_pickle(f"{self.save_path}/train_output.pkl")
+            test_pred_df.to_pickle(f"{self.save_path}/test_output.pkl")
+
+        print("-- Done.")
